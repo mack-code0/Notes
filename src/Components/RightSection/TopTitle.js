d2580a755a43d8b4f736693173d2c02fb109e333
mack-code0 <macaulayhilbert202@gmail.com>
2022-05-29 22:39:30 +0800

    added dropdown for sorting notes

--- src/Components/RightSection/TopTitle.js
+++ src/Components/RightSection/TopTitle.js
@@ -1,14 +1,31 @@
+import { useState } from "react"
 
 function TopTitle({ newNoteViewHandler, setListView, listView }) {
+    const [sortDropdown, setSortDropdown] = useState(false)
+    const [selectedOption, setSelectedOption] = useState("")
+
+    const selectHandler = (e) => {
+        setSelectedOption(e.target.innerHTML)
+        setSortDropdown(false)
+    }
+
     return (
         <div className="topTitle mt-4 p-4">
             <div className="row">
                 <h5 className="col-12 col-md-2">Notes</h5>
                 <div className="col-12 col-md-10 mt-3 mt-md-0 d-flex justify-content-md-end">
-                    <select className="mr-3">
-                        <option className="p-3">LastAdded</option>
-                        <option>Last Added</option>
-                    </select>
+                    <div className="select d-flex align-items-center justify-content-between px-4 mr-3">
+                        <span>{selectedOption ? selectedOption : "Last Added"}</span><img onClick={() => setSortDropdown(!sortDropdown)} alt="" width="40" src="https://ik.imagekit.io/36h35rdxx/arrow-drop-down_fbeflyvky.png?ik-sdk-version=javascript-1.4.3&updatedAt=1653832685301" />
+                        <div className={`options ${sortDropdown ? "d-flex" : "d-none"} flex-column`}>
+                            <span className="pt-3 px-3">Sort by</span>
+                            <span onClick={selectHandler} className="pt-3 px-3 optionItem">Category</span>
+                            <span onClick={selectHandler} className="pt-3 px-3 optionItem">Employee</span>
+                            <span onClick={selectHandler} className="py-3 px-3 optionItem">Shared with me</span>
+                            <span className="pt-3 px-3">Order</span>
+                            <span onClick={selectHandler} className="pt-3 px-3 optionItem">Alphabetical</span>
+                            <span onClick={selectHandler} className="py-3 px-3 optionItem">Last Added</span>
+                        </div>
+                    </div>
                     <button onClick={() => newNoteViewHandler(true)} className="defaultBtn">Add New Note</button>
                 </div>
             </div>
